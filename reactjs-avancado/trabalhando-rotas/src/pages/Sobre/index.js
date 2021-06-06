939b27a18b602c89e9040d4f33c226ceb3f592cc
José Luiz <luizbsilva@gmail.com>
2021-06-06 12:19:41 -0300

    React Advanced section
    Finished React Advanced section
    Completed classes:
    - HTTP requests;
    - Working with Routes;
    - Improving our Routes;

---
+++ reactjs-avancado/trabalhando-rotas/src/pages/Sobre/index.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom';
+
+export default function Sobre() {
+  return (
+    <div>
+      <h1>Sobre o curso Reactjs...</h1><br/>
+
+      <Link to="/" >Home</Link> <br/>
+      <Link to="/contato">Contatos</Link>
+    </div>
+  );
+ }
